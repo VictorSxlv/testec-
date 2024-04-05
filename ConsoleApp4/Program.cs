e491db72231c7af1d4de178228b6679c5bd85cab
Victor Silva <victor.sxlv@gmail.com>
2024-04-05 09:58:08 -0300

    Teste de repositorio git

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp4;
+using System.Collections.Generic;
+
+Console.Write("HOW MANY EMPLOYEES WILL BE REGISTERED? ");
+int n = int.Parse(Console.ReadLine());
+List<employee> funcionarios = new List<employee>();
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("EMPLOYEE #" + (i + 1));
+    Console.Write("ID: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("NAME: ");
+    string name = Console.ReadLine();
+    Console.Write("SALARY: ");
+    double salary = double.Parse(Console.ReadLine());
+
+    funcionarios.Add(new employee(id, name, salary));
+    Console.WriteLine();
+}
+
+Console.Write("ENTER THE EMPLYEE ID THAT WILL HAVE SALARY INCREASE: ");
+int searchid = int.Parse(Console.ReadLine());
+
+employee emp = funcionarios.Find(X => X.Id == searchid);
+
+if (emp != null)
+{
+
+
+    Console.Write("ENTER THE INCREASE PERCENTAGE: ");
+    double porcentagem = int.Parse(Console.ReadLine());
+
+    emp.IncreaseSalary(porcentagem);
+
+}
+else Console.WriteLine("THIS ID IS NOT VALID!");
+
+Console.WriteLine();
+Console.WriteLine("UPDATED LIST OF EMPLOYEES: ");
+Console.WriteLine();
+
+foreach (employee obj in funcionarios) Console.WriteLine(obj);
---
+++ ConsoleApp4/employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+     class employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public employee(int id, string name, double salary) {
+        
+            Id = id;
+            Name = name;    
+            Salary = salary;
+        
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += (percentage / 100) * Salary;
+
+        }
+
+        public override string ToString()
+        {
+            return Id +
+                ", " +
+                Name +
+                ", " +
+                Salary.ToString("F2");
+
+        }
+
+    }
+}
